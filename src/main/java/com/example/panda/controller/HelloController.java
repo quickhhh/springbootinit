dc40c0e522c21aff4dd0494126778be03d9617b0
quickhelper <574346727@qq.com>
2018-09-18 10:43:14 +0800

    错误处理

--- src/main/java/com/example/panda/controller/HelloController.java
+++ src/main/java/com/example/panda/controller/HelloController.java
@@ -3,6 +3,7 @@ package com.example.panda.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.text.DateFormat;
 import java.util.Date;
@@ -10,8 +11,11 @@ import java.util.Date;
 @Controller
 public class HelloController {
     @RequestMapping("/hello")
-    public String hello(Model m) {
+    public String hello(Model m, @RequestParam(value = "isbad",required = false) String isbad) throws Exception {
         m.addAttribute("now", DateFormat.getDateTimeInstance().format(new Date()));
+        if("bad".equals(isbad)){
+            throw new Exception("bad");
+        }
         m.addAttribute("name","jone12321");
         return "hello";
     }
