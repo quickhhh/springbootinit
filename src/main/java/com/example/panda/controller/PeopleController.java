f61a57c140026fc344a4e807c910d987758fa512
quickhelper <574346727@qq.com>
2018-09-18 15:23:16 +0800

    mybatis分页优化

--- src/main/java/com/example/panda/controller/PeopleController.java
+++ src/main/java/com/example/panda/controller/PeopleController.java
@@ -2,10 +2,13 @@ package com.example.panda.controller;
 
 import com.example.panda.mapper.PeopleMapper;
 import com.example.panda.pojo.People;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -14,13 +17,45 @@ public class PeopleController {
     @Autowired
     PeopleMapper peopleMapper;
 
-    @RequestMapping("/listPeople")
-    public String listCategory(Model m) throws Exception {
+    /*@RequestMapping("/listPeople")
+    public String listPeople(Model m) throws Exception {
         List<People> cs=peopleMapper.findAll();
 
         m.addAttribute("cs", cs);
 
         return "listPeople";
+    }*/
+
+    @RequestMapping("/listPeople")
+    public String listCategory(Model m, @RequestParam(value = "start", defaultValue = "0") int start, @RequestParam(value = "size", defaultValue = "5") int size) throws Exception {
+        PageHelper.startPage(start,size,"id desc");
+        List<People> cs=peopleMapper.findAll();
+        PageInfo<People> page = new PageInfo<>(cs);
+        m.addAttribute("page", page);
+
+        return "listPeople";
     }
 
+
+    @RequestMapping("/addPeople")
+    public String listPeople(People c) throws Exception {
+        peopleMapper.save(c);
+        return "redirect:listPeople";
+    }
+    @RequestMapping("/deletePeople")
+    public String deletePeople(People c) throws Exception {
+        peopleMapper.delete(c.getId());
+        return "redirect:listPeople";
+    }
+    @RequestMapping("/updatePeople")
+    public String updatePeople(People c) throws Exception {
+        peopleMapper.update(c);
+        return "redirect:listPeople";
+    }
+    @RequestMapping("/editPeople")
+    public String listPeople(int id,Model m) throws Exception {
+        People c= peopleMapper.get(id);
+        m.addAttribute("c", c);
+        return "editPeople";
+    }
 }
--- src/main/java/com/example/panda/mapper/PeopleMapper.java
+++ src/main/java/com/example/panda/mapper/PeopleMapper.java
@@ -1,14 +1,25 @@
 package com.example.panda.mapper;
 
 import com.example.panda.pojo.People;
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.*;
 
 import java.util.List;
 
 @Mapper
 public interface PeopleMapper {
 
-    //@Select("select * from people_ ")
+    @Select("select * from people_ ")
     List<People> findAll();
+
+    @Insert(" insert into people_ ( name ) values (#{name}) ")
+    public int save(People people);
+
+    @Delete(" delete from people_ where id= #{id} ")
+    public void delete(int id);
+
+    @Select("select * from people_ where id= #{id} ")
+    public People get(int id);
+
+    @Update("update people_ set name=#{name} where id=#{id} ")
+    public int update(People people);
 }
