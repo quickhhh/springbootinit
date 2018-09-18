91554b8759730452477c11bd5650b369f8d674dc
quickhelper <574346727@qq.com>
2018-09-18 10:35:28 +0800

    热部署

---
+++ src/main/java/com/example/panda/PandaApplication.java
@@ -0,0 +1,12 @@
+package com.example.panda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PandaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PandaApplication.class, args);
+	}
+}
---
+++ src/main/java/com/example/panda/controller/HelloController.java
@@ -0,0 +1,18 @@
+package com.example.panda.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.text.DateFormat;
+import java.util.Date;
+
+@Controller
+public class HelloController {
+    @RequestMapping("/hello")
+    public String hello(Model m) {
+        m.addAttribute("now", DateFormat.getDateTimeInstance().format(new Date()));
+        m.addAttribute("name","jone12321");
+        return "hello";
+    }
+}
